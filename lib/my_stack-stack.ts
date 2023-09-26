7481188b5123fd59326cb3bfefb032069d119c2e
EC2 Default User <ec2-user@ip-172-31-29-206.us-east-2.compute.internal>
2023-09-26 19:44:18 +0000

    My cdk Stack

--- lib/my_stack-stack.ts
+++ lib/my_stack-stack.ts
@@ -1,10 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+// import * as cdk from 'aws-cdk-lib';
+// import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class MyStackStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+// export class MyStackStack extends cdk.Stack {
+//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
 
     // The code that defines your stack goes here
 
@@ -12,5 +12,111 @@ export class MyStackStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'MyStackQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+//   }
+// }
+
+
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+
+
+export class MyStackStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create a VPC - Will creat VPC, subnets, IGW, Public RT & Routes, 
+    // Allow Dynamic IP, EIP, NATGW, Private RT, Destination for NATGW,
+    // 
+    const vpc = new ec2.Vpc(this, 'SarvanVPC', {
+      cidr: '10.20.0.0/16',
+      vpcName: 'SarvanVPC',
+    });
+
+    
+    const asg = new autoscaling.AutoScalingGroup(this, 'ASGSarvan', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage(),
+      vpcSubnets: vpc.selectSubnets({subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS}),
+    });
+
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'ALBSarvan', {
+          vpc,
+          internetFacing: true,
+          vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
+    });
+
+    
+    const listener = alb.addListener('Listener', {
+      port: 80,
+    });
+
+
+    listener.addTargets('Target', {
+      port: 80,
+      targets: [asg]
+    });
+
+    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+    
+    
+    const dbsg = new ec2.SecurityGroup(this, 'DatabaseSecurityGroup', {
+          vpc: vpc,
+          allowAllOutbound: true,
+          description: id + ' Database',
+          securityGroupName: 'SarvanRDSSG',
+    });
+
+
+    dbsg.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(3306), 'MySQL traffic');
+
+    const UserName = 'admin'
+    
+    const rdsSecret = new secretsmanager.Secret(this, 'RDSSecret', {
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: UserName }),
+        excludePunctuation: true,
+        includeSpace: false,
+        generateStringKey: 'metro123',
+        // excludeCharacters: "\"@/\\ '",
+        passwordLength: 30,
+      },
+    });
+
+
+    // const mysqlCredentials = rds.Credentials.fromSecret(
+    //       rdsSecret,
+    //       UserName,
+    //     );
+    
+
+
+    // Create an RDS instance
+    new rds.DatabaseInstance(this, 'MyRDSInstance', {
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_5_7_37 }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+      vpc,
+      securityGroups: [dbsg],
+      multiAz: true,
+      allocatedStorage: 20,
+      storageType: rds.StorageType.GP2,
+      // credentials: mysqlCredentials,
+      credentials: {
+        username: UserName,
+        password: rdsSecret.secretValue,
+      },
+    });
+    
+    
+    
+    
+
+
   }
 }
